24f1138d264539bb1acc6a454bb24b68892152b4
Gianluigi Spisto <luigi.spisto92@gmail.com>
2021-02-25 14:43:52 +0100

    aggiunti commenti

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
+//dichiarazione e definizione variabili
 var nome = prompt('Qual è il tuo nome?');
 var cognome = prompt('Qual è il tuo cognome?');
 var colore = prompt('Qual è il tuo colore preferito?');
 var password = nome + cognome + colore;
 
+//write line/print
 document.writeln(password + '27'); //1 modo
 document.writeln(nome + cognome + colore + '27'); //2 modo
 
