325451cecbdb49adb58589c51f23512cecc2c5ca
Gianluigi Spisto <luigi.spisto92@gmail.com>
2021-02-25 14:38:31 +0100

    Creato link per collegare js e aggiunto script per generare una password

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var nome = prompt('Qual è il tuo nome?');
+var cognome = prompt('Qual è il tuo cognome?');
+var colore = prompt('Qual è il tuo colore preferito?');
+var password = nome + cognome + colore;
+
+//document.writeln(password + '27'); //1 modo
+//document.writeln(nome + cognome + colore + '27'); //2 modo
+
+
+//Chicca per generare numero random
+
+var numero = Math.floor(Math.random() * 100);
+document.writeln(password + numero);
