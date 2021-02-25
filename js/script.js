5cc7d04aa82856747665d5bd63d14b0b54412e8a
Gianluigi Spisto <luigi.spisto92@gmail.com>
2021-02-25 14:40:58 +0100

    Aggiunto genera password

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ var cognome = prompt('Qual è il tuo cognome?');
 var colore = prompt('Qual è il tuo colore preferito?');
 var password = nome + cognome + colore;
 
-//document.writeln(password + '27'); //1 modo
-//document.writeln(nome + cognome + colore + '27'); //2 modo
+document.writeln(password + '27'); //1 modo
+document.writeln(nome + cognome + colore + '27'); //2 modo
 
 
 //Chicca per generare numero random
 
-var numero = Math.floor(Math.random() * 100);
-document.writeln(password + numero);
+//var numero = Math.floor(Math.random() * 100);
+//document.writeln(password + numero);
